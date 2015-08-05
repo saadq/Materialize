36072ad89c9d240666fe216b331d2ffe76a1ccc3
saadq <saad@saadquadri.com>
2015-08-05 11:20:03 -0400

    Add different versions of theme

---
+++ person.rb
@@ -0,0 +1,20 @@
+
+# This is a comment
+
+class Person
+
+  attr_accessor :name
+
+  def initialize(attributes = {})
+    @name = attributes[:name]
+  end
+
+  def self.greet
+    "hello"
+  end
+
+end
+
+person = Person.new(:name => "Saad")
+print Person::greet, " ", person1.name, "\n"
+puts "another #{Person::greet} #{person1.name}"
